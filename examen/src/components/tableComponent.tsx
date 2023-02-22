49b3d96550ffc1dc3caab144c3dbbf70aa35df55
Lisa Dünte <lisa.duente@gmail.com>
2023-02-22 13:26:30 +0100

    WIP tableComponent

---
+++ examen/src/components/tableComponent.tsx
@@ -0,0 +1,36 @@
+import Button from "react-bootstrap/esm/Button";
+import Table from "react-bootstrap/esm/Table";
+import { Columns } from "../types/Columns";
+
+export type columnProps = {
+    columns: Columns[];
+    tableName: string;
+}
+
+export default function tableComponent(props: columnProps){
+    const {columns, tableName} = props;
+
+    //useEffect here
+
+    return (
+        <div>
+            <Button className="backButton">Back</Button>
+            <Button className="createButton">Create column</Button>
+            <Table striped bordered hover>
+                <tr>
+                    {columns.map((data) => {
+                        return (
+                            <th>
+                                {data.columnName}
+                            </th>
+                        );
+                    })}
+                </tr>
+            </Table>
+        </div>
+       
+    )
+
+
+}
+
---
+++ examen/src/types/Columns.ts
@@ -0,0 +1,3 @@
+export type Columns = {
+    columnName: string;
+}
