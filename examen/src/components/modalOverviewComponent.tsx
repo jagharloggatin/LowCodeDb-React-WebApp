333c5343c917d0cea7b7e16af3d6b6c2be21fd7d
Ferri_Emin <ferri_emin@hotmail.com>
2023-02-28 14:34:52 +0100

    Update modalOverviewComponent.tsx

--- examen/src/components/modalOverviewComponent.tsx
+++ examen/src/components/modalOverviewComponent.tsx
@@ -1,66 +1,86 @@
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
-import {useState} from 'react';
+import { useState } from "react";
 import { TableData } from "../types/TableData";
 import { Columns } from "../types/Columns";
-import {FloatingLabel, Form} from 'react-bootstrap'
+import { FloatingLabel, Form } from "react-bootstrap";
 import { createDatabases } from "../controllers/createDatabase";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { NameData } from "../types/NameData";
-import {useParams} from "react-router-dom";
-
+import { useParams } from "react-router-dom";
 
 export type modalProps = {
-    nameTag: string;
-    showModal: boolean
-    setShowModal: React.Dispatch<React.SetStateAction<boolean>>;
-    addRows: React.Dispatch<React.SetStateAction<TableData[]>>;
-    rows: TableData[];
-    createDataHandler: Function;
-}
+  nameTag: string;
+  showModal: boolean;
+  setShowModal: React.Dispatch<React.SetStateAction<boolean>>;
+  addRows: React.Dispatch<React.SetStateAction<TableData[]>>;
+  rows: TableData[];
+  createDataHandler: Function;
+};
 
-export default function ModalComponent(props: modalProps){
+export default function ModalComponent(props: modalProps) {
+  const { nameTag, showModal, setShowModal, addRows, rows, createDataHandler } =
+    props;
+  const { register, handleSubmit } = useForm<NameData>();
+  const params = useParams();
 
-    const { nameTag, showModal, setShowModal, addRows, rows, createDataHandler } = props;
-    const { register, handleSubmit } = useForm<NameData>();
-    const params = useParams();
+  const onSubmit: SubmitHandler<NameData> = async (data) => {
+    
+    const trimName: string = data.name
+      .trim()
+      .replace(" ", "_")
+      .replace(/(\s|-|_|~)+/g, "_")
+      .toLowerCase();
 
-    const onSubmit: SubmitHandler<NameData> = async(data) => {
-        console.log(data.name)
-        console.log(params.databaseName)
-        const trimName: string = data.name.trim().replace(' ', '_').replace(/(\s|-|_|~)+/g, '_').toLowerCase();
+    const dbOrTableNames: string[] = [];
+    rows.map((row: TableData) => {
+        dbOrTableNames.push(Object.values(row)[0]);
+    });
 
-        if(data.name.length>0){
-            await createDataHandler(trimName, params.databaseName)
-            addRows([{name: trimName},...rows] as TableData[])
-        }else{
-            alert("Your database name needs at least one character!")
-        }
-        setShowModal(false);
-     };
+    if(dbOrTableNames.includes(data.name)){
+        alert('Name allready exists.')
+        return
+    }
 
-    return (
-        <div>
-            <Modal show={showModal}>
-                <Modal.Header>
-                    <Modal.Title>
-                        Create {nameTag}
-                    </Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                 <Form onSubmit={handleSubmit(onSubmit)}>
-                    <FloatingLabel controlId="floatingInput" label={nameTag + ' name'} className="mb-3">
-                        <Form.Control placeholder={nameTag + ' name'} type="text" {...register("name")}/>
-                    </FloatingLabel>
-                     <Button type="submit" variant="success">
-                         Create
-                    </Button>
-                    <Button variant="danger" onClick={()=>setShowModal(false)}>
-                        Cancel
-                    </Button>
-                 </Form>
-                </Modal.Body>
-            </Modal>
-        </div>
-    )
+    if (data.name.length > 0) {
+      await createDataHandler(trimName, params.databaseName);
+      addRows([{ name: trimName }, ...rows] as TableData[]);
+    }
+    else {
+      alert("Your database name needs at least one character!");
+    }
+
+    setShowModal(false);
+  };
+
+  return (
+    <div>
+      <Modal show={showModal}>
+        <Modal.Header>
+          <Modal.Title>Create {nameTag}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={handleSubmit(onSubmit)}>
+            <FloatingLabel
+              controlId="floatingInput"
+              label={nameTag + " name"}
+              className="mb-3"
+            >
+              <Form.Control
+                placeholder={nameTag + " name"}
+                type="text"
+                {...register("name")}
+              />
+            </FloatingLabel>
+            <Button type="submit" variant="success">
+              Create
+            </Button>
+            <Button variant="danger" onClick={() => setShowModal(false)}>
+              Cancel
+            </Button>
+          </Form>
+        </Modal.Body>
+      </Modal>
+    </div>
+  );
 }
